dff21015e1a129bed20c89570efc19f35684e4fd
Parham Alvani <parham.alvani@gmail.com>
2014-11-17 08:57:48 +0330

    gdt and idt is now supported.

---
+++ src/isr.c
@@ -0,0 +1,17 @@
+// In the name of god
+// isr.c -- High level interrupt service routines and interrupt request handlers.
+//	Part of this code is modified from Bran's kernel development tutorials.
+//	Rewritten for JamesM's kernel development tutorials.
+//	Rewritten for TOs kernel developmnet.
+//	17 November 2015
+
+#include "common.h"
+#include "isr.h"
+#include "monitor.h"
+
+// This gets called from our ASM interrupt handler stub.
+void isr_handler(registers_t regs){
+    monitor_write("Recieved interrupt: ");
+    monitor_write_dec(regs.int_no);
+    monitor_put('\n');
+}
