5c7c52af583dc501da3a3a91477e0edad2c64fbd
Parham Alvani <parham.alvani@gmial.com>
2014-11-20 06:23:55 +0330

    irq handling is added.
    for test irq handling, timer pit(programmable interval timer) is tested.

---
+++ src/timer.h
@@ -0,0 +1,14 @@
+// In the name of god
+// timer.h -- Defines the interface for all PIT-related functions.
+//	Written for JamesM's kernel development tutorials.
+//	Rewritten for TOs development.
+//	20 November 2015
+
+#ifndef TIMER_H
+#define TIMER_H
+
+#include "common.h"
+
+void init_timer(u32int frequency);
+
+#endif
