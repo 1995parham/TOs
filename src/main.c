a925fec7a80f2edd0be0e3e28630370fcb8c5a21
Parham Alvani <parham.alvani@gmail.com>
2014-12-01 08:20:42 +0330

    some little change done.

--- src/main.c
+++ src/main.c
@@ -1,22 +1,20 @@
 // In the name of god
 // main.c -- TOs kernel main c file
 //	Parham Alvani(parham.alvani@gmail.com)
-//	14 November 2015
+//	14 November 2014
 
 #include "monitor.h"
 #include "descriptor_tables.h"
 #include "timer.h"
 #include "multiboot.h"
 
-int main(multiboot_info_t *mboot_ptr)
-{
-	// All our initialisation calls will go in here.
+int main(multiboot_info_t *mboot_ptr) {
 	monitor_clear();
 
 	monitor_write("Welcome to TOs(Tiny OS) written by\n");
 	monitor_write("Parham Alvani(parham.alvani@gmail.com)\n");
-	monitor_write("Navid Mashayekhi\n");
-	monitor_write("Mohammad Reza Mahboubi Ardakani\n");
+	monitor_write("Navid Mashayekhi(navidmsk@gmail.com)\n");
+	monitor_write("Mohammad Reza Mahboubi Ardakani(mrmahboubi@gmail.com)\n");
 	
 	init_descriptor_tables();
 	
