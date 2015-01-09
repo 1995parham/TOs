459a63a4ebe9b5fa8351335c278094263fa87bd1
Parham Alvani <parham.alvani@gmial.com>
2015-01-09 06:35:25 +0330

    Start reformatting all codes. Part #6.

--- src/lib/monitor.c
+++ src/lib/monitor.c
@@ -5,7 +5,7 @@
  *
  * [] Creation Date : 27-12-2014
  *
- * [] Last Modified : Tue 06 Jan 2015 09:13:14 AM IRST
+ * [] Last Modified : Fri 09 Jan 2015 06:34:41 AM IRST
  *
  * [] Created By : Parham Alvani (parham.alvani@gmail.com)
  * =======================================
@@ -24,12 +24,16 @@ uint8_t cursor_y = 0;
 */
 static void move_cursor(void)
 {
-    	/* The screen is 80 characters wide... */
-    	uint16_t cursorLocation = cursor_y * 80 + cursor_x;
-    	outb(0x3D4, 14);                  /* Tell the VGA board we are setting the high cursor byte. */
-    	outb(0x3D5, cursorLocation >> 8); /* Send the high cursor byte. */
-    	outb(0x3D4, 15);                  /* Tell the VGA board we are setting the low cursor byte. */
-    	outb(0x3D5, cursorLocation);      /* Send the low cursor byte. */
+	/* The screen is 80 characters wide... */
+	uint16_t cursorLocation = cursor_y * 80 + cursor_x;
+	/* Tell the VGA board we are setting the high cursor byte. */
+	outb(0x3D4, 14);
+	/* Send the high cursor byte. */
+	outb(0x3D5, cursorLocation >> 8);
+	/* Tell the VGA board we are setting the low cursor byte. */
+	outb(0x3D4, 15);
+	/* Send the low cursor byte. */
+	outb(0x3D5, cursorLocation);
 }
 
 /*
@@ -46,15 +50,14 @@ static void scroll(void)
         	// Move the current text chunk that makes up the screen
         	// back in the buffer by a line
         	int i;
-        	for (i = 0 * 80; i < 24 * 80; i++) {
-           		video_memory[i] = video_memory[i+80];
-        	}
+        	for (i = 0 * 80; i < 24 * 80; i++)
+           		video_memory[i] = video_memory[i + 80];
 
         	// The last line should now be blank. Do this by writing
         	// 80 spaces to it.
-        	for (i = 24 * 80; i < 25 * 80; i++) {
+        	for (i = 24 * 80; i < 25 * 80; i++)
             		video_memory[i] = blank;
-        	}
+
         	// The cursor should now be on the last line.
         	cursor_y = 24;
    	}
@@ -79,49 +82,52 @@ void putc(char c)
 	 * |Backcolor|Forecolor|
 	*/
     	uint8_t  attributeByte = (backColour << 4) | (foreColour & 0x0F);
-    	// The attribute byte is the top 8 bits of the word we have to send to the
-    	// VGA board.
+    	/*
+	 * The attribute byte is the top 8 bits of the word we have to send to the
+    	 * VGA board.
+	*/
 	uint16_t attribute = attributeByte << 8;
     	uint16_t *location;
 
-    	// Handle a backspace, by moving the cursor back one space
-    	if (c == 0x08 && cursor_x){
+    	/* Handle a backspace, by moving the cursor back one space */
+    	if (c == 0x08 && cursor_x) {
         	cursor_x--;
-    	}
-
-    	// Handle a tab by increasing the cursor's X, but only to a point
-    	// where it is divisible by 8.
+	}
+    	/*
+	 * Handle a tab by increasing the cursor's X, but only to a point
+    	 * where it is divisible by 8.
+	*/
     	else if (c == 0x09) {
         	cursor_x = (cursor_x + 8) & ~(8 - 1);
-    	}
-
-    	// Handle carriage return
-    	else if (c == '\r') {
+	}
+	/* Handle carriage return */
+	else if (c == '\r') {
      		cursor_x = 0;
-    	}
-
-	// Handle newline by moving cursor back to left and increasing the row
+	}
+	/* Handle newline by moving cursor back to left and increasing the row */
    	else if (c == '\n') {
         	cursor_x = 0;
         	cursor_y++;
-    	}
-	// Handle any other printable character.
+	}
+	/* Handle any other printable character. */
     	else if (c >= ' ') {
         	location = video_memory + (cursor_y * 80 + cursor_x);
         	*location = c | attribute;
         	cursor_x++;
     	}
 
-    	// Check if we need to insert a new line because we have reached the end
-    	// of the screen.
+    	/*
+	 * Check if we need to insert a new line because we have reached the end
+    	 * of the screen.
+	*/
     	if (cursor_x >= 80) {
         	cursor_x = 0;
         	cursor_y ++;
 	}
 
-    	// Scroll the screen if needed.
+    	/* Scroll the screen if needed. */
     	scroll();
-    	// Move the hardware cursor.
+    	/* Move the hardware cursor. */
     	move_cursor();
 }
 
@@ -143,69 +149,11 @@ void cls(void){
     	move_cursor();
 }
 
-// Outputs a null-terminated ASCII string to the monitor.
+/* Outputs a null-terminated ASCII string to the monitor. */
 void puts(char *c)
 {
     	int i = 0;
     	while (c[i]){
         	putc(c[i++]);
     	}
-}
-
-void monitor_write_hex(uint32_t n){
-	int32_t tmp;
-	
-	puts("0x");
-	
-	char noZeroes = 1;
-
-	int i;
-	for (i = 28; i > 0; i -= 4){
-		tmp = (n >> i) & 0xF;
-		if (tmp == 0 && noZeroes != 0){
-			continue;
-		}
-
-		if (tmp >= 0xA){
-			noZeroes = 0;
-			putc(tmp-0xA+'a' );
-		}else{
-			noZeroes = 0;
-			putc( tmp+'0' );
-		}
-	}
-
-	tmp = n & 0xF;
-	if (tmp >= 0xA){
-		putc(tmp-0xA+'a');
-	}
-	else{
-		putc(tmp+'0');
-	}
-}
-
-void monitor_write_dec(uint32_t n){
-	if (n == 0){
-		putc('0');
-		return;
-	}
-
-	int32_t acc = n;
-	char c[32];
-	int i = 0;
-	while (acc > 0){
-		c[i] = '0' + acc%10;
-		acc /= 10;
-		i++;
-	}
-	c[i] = 0;
-
-	char c2[32];
-	c2[i--] = 0;
-	int j = 0;
-	while(i >= 0){
-		c2[i--] = c[j++];
-	}
-	puts(c2);
-
 }
