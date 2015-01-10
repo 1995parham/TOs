ef1488792793b7a0d0f627f02b9eb7f803a11006
Parham Alvani <parham.alvani@gmail.com>
2015-01-10 18:41:12 +0330

    Start reformatting all codes. Part #10.

--- src/lib/io.c
+++ src/lib/io.c
@@ -5,7 +5,7 @@
  *
  * [] Creation Date : 09-01-2015
  *
- * [] Last Modified : Fri 09 Jan 2015 06:29:04 PM IRST
+ * [] Last Modified : Fri 09 Jan 2015 09:54:30 PM IRST
  *
  * [] Created By : Parham Alvani (parham.alvani@gmail.com)
  * =======================================
@@ -18,15 +18,35 @@
  * 	mov al, value
  * 	mov dx, port
  * 	out dx, al
- */
+*/
 void outb(uint16_t port, uint8_t value)
 {
-	__asm__("outb %0, %1"
+	__asm__("outb %0,%1"
 		:
 		: "a" (value), "d" (port)
 	       );
 }
 
+/*
+ * NASM:
+ * 	mov al, value
+ * 	mov dx, port
+ * 	out dx, al
+*/
+void outw(uint16_t port, uint16_t value)
+{
+	__asm__("outw %0,%1"
+		:
+		: "a" (value), "d" (port)
+	       );
+}
+
+/*
+ * NASM:
+ * 	mov al, value
+ * 	mov dx, port
+ * 	out dx, al
+*/
 uint8_t inb(uint16_t port)
 {
 	register uint8_t __res;
@@ -37,6 +57,12 @@ uint8_t inb(uint16_t port)
 	return __res;
 }
 
+/*
+ * NASM:
+ * 	mov al, value
+ * 	mov dx, port
+ * 	out dx, al
+*/
 uint16_t inw(uint16_t port)
 {	
 	register uint8_t __res;
