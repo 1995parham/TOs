b7c29946872283bcae9fdbfa1b814b84abf68916
Parham Alvani <parham.alvani@gmail.com>
2015-01-31 13:15:27 +0330

    Start reformatting all codes. Part #13.

--- src/include/string.h
+++ src/include/string.h
@@ -5,7 +5,7 @@
  *
  * [] Creation Date : 06-01-2015
  *
- * [] Last Modified : Fri Jan 30 23:40:11 2015
+ * [] Last Modified : Sat 31 Jan 2015 01:14:47 PM IRST
  *
  * [] Created By : Parham Alvani (parham.alvani@gmail.com)
  * =======================================
@@ -537,65 +537,68 @@ extern inline char *strtok(char *s, const char *ct)
 /*
  * Changes by falcon<zhangjinw@gmail.com>, the original return value is static
  * inline ... it can not be called by other functions in another files.
- */
+*/
 
 extern inline void *memcpy(void *dest, const void *src, int n)
 {
-__asm__ ("cld\n\t"
-	"rep\n\t"
-	"movsb"
-	::"c" (n),"S" (src),"D" (dest)
-	);
-return dest;
+	__asm__ ("cld\n\t"
+		"rep\n\t"
+		"movsb"
+		:
+		:"c" (n),"S" (src),"D" (dest)
+		);
+	return dest;
 }
 
 extern inline void *memmove(void *dest, const void *src, int n)
 {
-if (dest < src)
-__asm__("cld\n\t"
-	"rep\n\t"
-	"movsb"
-	::"c" (n),"S" (src),"D" (dest)
-	);
-else
-__asm__("std\n\t"
-	"rep\n\t"
-	"movsb"
-	::"c" (n),"S" (src+n-1),"D" (dest+n-1)
-	);
-return dest;
+	if (dest < src)
+		__asm__("cld\n\t"
+			"rep\n\t"
+			"movsb"
+			:
+			:"c" (n),"S" (src),"D" (dest)
+			);
+	else
+		__asm__("std\n\t"
+		"rep\n\t"
+		"movsb"
+		:
+		:"c" (n),"S" (src+n-1),"D" (dest+n-1)
+		);
+	return dest;
 }
 
 static inline int memcmp(const void *cs, const void *ct, int count)
 {
-register int __res ;
-__asm__("cld\n\t"
-	"repe\n\t"
-	"cmpsb\n\t"
-	"je 1f\n\t"
-	"movl $1,%%eax\n\t"
-	"jl 1f\n\t"
-	"negl %%eax\n"
-	"1:"
-	:"=a" (__res):"0" (0),"D" (cs),"S" (ct),"c" (count)
-	);
-return __res;
+	register int __res ;
+	__asm__("cld\n\t"
+		"repe\n\t"
+		"cmpsb\n\t"
+		"je 1f\n\t"
+		"movl $1,%%eax\n\t"
+		"jl 1f\n\t"
+		"negl %%eax\n"
+		"1:"
+		:"=a" (__res):"0" (0),"D" (cs),"S" (ct),"c" (count)
+		);
+	return __res;
 }
 
 extern inline void *memchr(const void *cs, char c, int count)
 {
-register void *__res ;
-if (!count)
-	return NULL;
-__asm__("cld\n\t"
-	"repne\n\t"
-	"scasb\n\t"
-	"je 1f\n\t"
-	"movl $1,%0\n"
-	"1:\tdecl %0"
-	:"=D" (__res):"a" (c),"D" (cs),"c" (count)
-	);
-return __res;
+	register void *__res ;
+	if (!count)
+		return NULL;
+	__asm__("cld\n\t"
+		"repne\n\t"
+		"scasb\n\t"
+		"je 1f\n\t"
+		"movl $1,%0\n"
+		"1:\tdecl %0"
+		:"=D" (__res):"a" (c),"D" (cs),"c" (count)
+		);
+	return __res;
 }
 
 /*
